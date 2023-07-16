625f6af6db0141be3bbc6b7ed469949d670ca96f
Honma Masaru <46467282+HonmaMasaru@users.noreply.github.com>
2023-07-16 11:49:10 +0900

    Initial Commit

---
+++ Boids/BoidsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BoidsApp.swift
+//  Boids
+//
+//  Created by Honma Masaru on 2023/07/16.
+//
+
+import SwiftUI
+
+@main
+struct BoidsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Boids/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Boids
+//
+//  Created by Honma Masaru on 2023/07/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
